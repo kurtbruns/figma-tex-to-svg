584e391f57611e977afcafd5f913c670fd02bcda
Kurt Bruns <contact@kurtbruns.com>
2023-06-27 15:35:45 -0600

    Scroll and zoom into view embedded SVG

--- src/code.ts
+++ src/code.ts
@@ -3,6 +3,14 @@ figma.showUI(__html__)
 figma.ui.resize(360, 360);
 
 figma.ui.onmessage = (pluginMessage) => {
-  figma.createNodeFromSvg(pluginMessage.svg).rescale(1.5)
+
+  const nodes: SceneNode [] = [];
+  let svg = figma.createNodeFromSvg(pluginMessage.svg);
+  nodes.push(svg)
+  svg.rescale(1.5);
+
+  figma.currentPage.selection = nodes;
+  figma.currentPage.appendChild(svg)
+  figma.viewport.scrollAndZoomIntoView(nodes)
 }
 
