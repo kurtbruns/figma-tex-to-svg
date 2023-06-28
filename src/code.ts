bb2607021738c5a51e6f4ec0007a28b6ee28680a
Kurt Bruns <contact@kurtbruns.com>
2023-06-28 08:21:52 -0600

    Now renders the math expression in the center of the current viewport

--- src/code.ts
+++ src/code.ts
@@ -8,10 +8,14 @@ figma.ui.onmessage = (pluginMessage) => {
   let svg = figma.createNodeFromSvg(pluginMessage.svg);
   svg.rescale(1.5);
   svg.name = pluginMessage.tex;
+
+  // Center the frame in our current viewport so we can see it.
+  svg.x = figma.viewport.center.x - svg.width / 2;
+  svg.y = figma.viewport.center.y - svg.height / 2
   nodes.push(svg)
 
   figma.currentPage.selection = nodes;
   figma.currentPage.appendChild(svg)
-  figma.viewport.scrollAndZoomIntoView(nodes)
+  // figma.viewport.scrollAndZoomIntoView(nodes)
 }
 
