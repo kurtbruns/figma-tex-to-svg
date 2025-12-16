8f369c6e9e7cdad157352d00ff250c750b3aa7f2
Kurt Bruns <12160501+kurtbruns@users.noreply.github.com>
2025-12-16 15:26:48 -0700

    Try staging the manifest

--- sync-version.js
+++ sync-version.js
@@ -1,11 +1,16 @@
 // sync-version.js
 const pkg = require('./package.json');
 const manifest = require('./manifest.json');
+const fs = require('fs');
+const { execSync } = require('child_process');
 
 manifest.version = pkg.version;
 
-require('fs').writeFileSync(
+fs.writeFileSync(
   './manifest.json',
   JSON.stringify(manifest, null, 2) + '\n'
 );
+
+// Stage manifest.json so it's included in the npm version commit
+execSync('git add manifest.json', { stdio: 'inherit' });
 
